b978991f44d09f310e5a3dc39aabd948bcff8ed5
Pedro Lucas <pedrolucas1014zz@gmail.com>
2022-06-07 11:30:07 -0300

    Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css";
 import React from "react";
 // Importando CSS
 import "./index.css";
@@ -7,14 +8,25 @@ import Primeiro from "./components/basic/Primeiro.jsx";
 import ComParametro from "./components/basic/ComParametro";
 import Fragmento from "./components/basic/Fragmento";
 import Aleatorio from "./components/basic/Aleatorio";
+import Card from "./components/layout/Card";
 
 export default () => (
-  <div id="app">
+  <div className="App">
     <h1>Fundamentos React</h1>
-    <Aleatorio min={1} max={60} />
-    <Fragmento />
-    {/* passando propriedades */}
-    <ComParametro titulo="Situação do aluno" aluno="Pedro" nota="4.5" />
-    <Primeiro></Primeiro>
+    <div className="Cards">
+      <Card title="#04 - Desafio Aleatorio" color="#1F4690">
+        <Aleatorio min={1} max={60} />
+      </Card>
+      <Card title="#03 - Fragmento" color="#3A5BA0">
+        <Fragmento />
+      </Card>
+      <Card title="#02 - Com Parametro" color="#FFA500">
+        {/* passando propriedades */}
+        <ComParametro titulo="Situação do aluno" aluno="Pedro" nota="4.5" />
+      </Card>
+      <Card title="#01 - Primeiro Componete" color="#4C3575">
+        <Primeiro></Primeiro>
+      </Card>
+    </div>
   </div>
 );
