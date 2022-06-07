92d66df7c8b2db86925d1af91c460c68fb29dbe9
Pedro Lucas <pedrolucas1014zz@gmail.com>
2022-06-07 09:18:57 -0300

    Imports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+// Importando react dom para poder usar os elementos dom
 import ReactDOM from "react-dom";
+// Importando react para poder usar JSX
+import React from "react";
+// Importando CSS
+import "./index.css";
 
-ReactDOM.render("Ola react", document.getElementById("root"));
+
+const tag = <strong>Ola react</strong>;
+
+ReactDOM.render(<div>{tag}</div>, document.getElementById("root"));
