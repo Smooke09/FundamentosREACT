62e64ff61e7486c166255f1cb0f7cf3b68575fab
Smooke09 <pedrolucas1014zz@gmail.com>
2022-06-08 11:02:57 -0300

    Indirect Comunication

--- src/components/comunicacao/IndiretaPai.jsx
+++ src/components/comunicacao/IndiretaPai.jsx
@@ -2,11 +2,29 @@ import React from "react";
 import IndiretaFilho from "./IndiretaFilho";
 
 export default (props) => {
+  let nome = "?";
+  let idade = 0;
+  let nerd = false;
+
   // nome idade nerd
+  function fornecerInformacoes(nomePar, idadePar, nerdPar) {
+    nome = nomePar;
+    idade = idadePar;
+    nerd = nerdPar;
+      
+    console.log(nome, idade, nerd);
+  }
 
   return (
     <div>
-      Pai <IndiretaFilho></IndiretaFilho>
+      <div>
+        <span>{nome} </span>
+        <span>
+          <strong>{idade} </strong>
+        </span>
+        <span>{nerd ? "Verdadeiro" : "Falso"}</span>
+      </div>
+      <IndiretaFilho quandoClicar={fornecerInformacoes}></IndiretaFilho>
     </div>
   );
 };
